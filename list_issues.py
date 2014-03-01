05126b3898d9faeaa727162ecb8bcfea55dbf632
Pete Graham <petegraham1@gmail.com>
2014-03-01 17:37:48 +0000

    Filtering issues returned now

--- list_issues.py
+++ list_issues.py
@@ -1,15 +1,23 @@
 import os
 from github import Github
 
+
 repo = 'LostProperty/dove-ath'
-label = 'cms' #'feedback'
+filter_label_text =  'CMS'
 token = os.getenv('FEEDBACK_GITHUB_TOKEN')
 
 g = Github(token)
 github_repo = g.get_repo(repo)
-# TODO: filter on tag here
-issues = github_repo.get_issues()
+labels = github_repo.get_labels()
+#labels = github_repo.get_labels(name='CMS') # Not supported, add a patch
 
+for label in labels:
+    if label.name == filter_label_text:
+        filter_label = label
+
+# Is it possible to filter by labels without having to call get labels first?
+# Dig in the code to see what is required to create the label object
+issues = github_repo.get_issues(labels=[filter_label])
 for issue in issues:
     # TODO: check if getting assignee is causing another network request
     # try:
