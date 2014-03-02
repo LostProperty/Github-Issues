98c66bdf87f147ad20d2b98d6a375e1fc2035861
Pete Graham <petegraham1@gmail.com>
2014-03-02 11:31:19 +0000

    Label exists check

--- list_issues.py
+++ list_issues.py
@@ -1,19 +1,25 @@
 import os
+import sys
+
 from github import Github
 
 
 repo = 'LostProperty/dove-ath'
-filter_label_text =  'CMS'
+filter_label_text =  'Feedback' #CMS'
+filter_label = False
 token = os.getenv('FEEDBACK_GITHUB_TOKEN')
 
 g = Github(token)
 github_repo = g.get_repo(repo)
 labels = github_repo.get_labels()
-#labels = github_repo.get_labels(name='CMS') # Not supported, add a patch
+#labels = github_repo.get_labels(name='CMS') # Not supported, add add patch
 
 for label in labels:
     if label.name == filter_label_text:
         filter_label = label
+
+if filter_label == False:
+    sys.exit('Error: Could not find label "{0}"'.format(filter_label_text))
 
 # Is it possible to filter by labels without having to call get labels first?
 # Dig in the code to see what is required to create the label object
