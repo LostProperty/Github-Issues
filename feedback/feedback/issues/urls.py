01c8cde17b9ea30a7fed890c621109874ce55df9
Pete Graham <petegraham1@gmail.com>
2014-08-04 18:00:09 +0100

    Image attachements showing now.

--- feedback/feedback/issues/urls.py
+++ feedback/feedback/issues/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, url
 from django.contrib.auth.views import login
 
@@ -15,5 +16,15 @@ urlpatterns = patterns(
     url(r'^add', add_issue, name='add_issue'),
     url(r'^login$', login,
         {'template_name': 'login.html'}, name='login'),
-    url(r'^logout$', logout_view, name='logout'),
+    url(r'^logout$', logout_view, name='logout')
+)
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+        # url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+        #     'document_root': settings.STATIC_ROOT,
+        # }),
 )
