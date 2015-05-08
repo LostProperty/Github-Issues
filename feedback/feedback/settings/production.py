f6ef64f7f9ddf2e5a6b6eff594bcf0caa03e61da
Pete Graham <petegraham1@gmail.com>
2015-05-08 12:44:53 +0100

    Changing production settings for provisioning test

--- feedback/feedback/settings/production.py
+++ feedback/feedback/settings/production.py
@@ -29,16 +29,16 @@ ALLOWED_HOSTS = []
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host
-EMAIL_HOST = environ.get('EMAIL_HOST', 'smtp.gmail.com')
+#EMAIL_HOST = environ.get('EMAIL_HOST', 'smtp.gmail.com')
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host-password
-EMAIL_HOST_PASSWORD = environ.get('EMAIL_HOST_PASSWORD', '')
+#EMAIL_HOST_PASSWORD = environ.get('EMAIL_HOST_PASSWORD', '')
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host-user
-EMAIL_HOST_USER = environ.get('EMAIL_HOST_USER', 'your_email@example.com')
+#EMAIL_HOST_USER = environ.get('EMAIL_HOST_USER', 'your_email@example.com')
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#email-port
-EMAIL_PORT = environ.get('EMAIL_PORT', 587)
+#EMAIL_PORT = environ.get('EMAIL_PORT', 587)
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
 EMAIL_SUBJECT_PREFIX = '[%s] ' % SITE_NAME
@@ -63,5 +63,5 @@ CACHES = {}
 
 ########## SECRET CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
-SECRET_KEY = get_env_setting('SECRET_KEY')
+#SECRET_KEY = get_env_setting('SECRET_KEY')
 ########## END SECRET CONFIGURATION
--- todo.md
+++ todo.md
@@ -1,5 +1,10 @@
+ - Ansible Am I A Man
+ - Riccardo contact
+ - Python companies in London
+***
+
  - About page
  - Code clean-up (mvp.py)
+ - Create server (Ansible)
  - Tighten permissions
  - Run Python3?
- - Ansible
