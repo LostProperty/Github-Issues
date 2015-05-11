d99e1e89e03c620fbba4e48785680a880e23a8f4
Pete Graham <petegraham1@gmail.com>
2015-05-11 17:44:20 +0100

    Prod settings to match dev

--- feedback/feedback/settings/production.py
+++ feedback/feedback/settings/production.py
@@ -1,62 +1,13 @@
-"""Production settings and globals."""
+"""Prod site settings."""
 
 from __future__ import absolute_import
 
-from os import environ
+from os.path import join
 
 from .base import *
 
-# Normally you should not import ANYTHING from Django directly
-# into your settings, but ImproperlyConfigured is an exception.
-from django.core.exceptions import ImproperlyConfigured
-
-
-def get_env_setting(setting):
-    """ Get the environment setting or return exception """
-    try:
-        return environ[setting]
-    except KeyError:
-        error_msg = "Set the %s env variable" % setting
-        raise ImproperlyConfigured(error_msg)
-
-STATIC_ROOT = '/srv/github_issues/static/'
-MEDIA_ROOT = '/srv/github_issues/uploads/'
-
-# Tmp debug for production server
 DEBUG = True
 
-########## HOST CONFIGURATION
-# See: https://docs.djangoproject.com/en/1.5/releases/1.5/#allowed-hosts-required-in-production
-ALLOWED_HOSTS = '*'
-########## END HOST CONFIGURATION
-
-########## EMAIL CONFIGURATION
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host
-EMAIL_HOST = environ.get('EMAIL_HOST', 'smtp.gmail.com')
-
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host-password
-EMAIL_HOST_PASSWORD = environ.get('EMAIL_HOST_PASSWORD', '')
-
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host-user
-EMAIL_HOST_USER = environ.get('EMAIL_HOST_USER', 'your_email@example.com')
-
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-port
-EMAIL_PORT = environ.get('EMAIL_PORT', 587)
-
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
-EMAIL_SUBJECT_PREFIX = '[%s] ' % SITE_NAME
-
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-use-tls
-EMAIL_USE_TLS = True
-
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#server-email
-SERVER_EMAIL = EMAIL_HOST_USER
-########## END EMAIL CONFIGURATION
-
-########## DATABASE CONFIGURATION
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -68,28 +19,5 @@ DATABASES = {
         'TEST_NAME': 'feedback_test',
     }
 }
-########## END DATABASE CONFIGURATION
 
-
-########## CACHE CONFIGURATION
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#caches
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-    }
-}
-########## END CACHE CONFIGURATION
-
-########## SECRET CONFIGURATION
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
-SECRET_KEY = 'asdfsadasdfsadf234234234' #get_env_setting('SECRET_KEY')
-########## END SECRET CONFIGURATION
-
-# Attempt to fix templates issues
-import os
-PROJECT_ROOT = os.path.dirname(os.path.realpath(os.path.join(__file__, '..')))
-here = lambda *x: os.path.realpath(os.path.join(PROJECT_ROOT, *x))
-
-TEMPLATE_DIRS = (
-    here('templates'),
-)
+STATIC_ROOT = '/srv/github_issues/static/'
