c5f1d6724d16a501f9398818d76d23cd85ba9898
Pete Graham <petegraham1@gmail.com>
2015-05-11 11:26:44 +0100

    setup.py changes

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup, find_packages
+from setuptools import setup #, find_packages
 
 
 here = os.path.abspath(os.path.dirname(__file__))
@@ -7,15 +7,15 @@ README = open(os.path.join(here, 'README.rst')).read()
 
 setup(name='feedback',
       version='0.1.0',
+      packages=['feedback'],
+      include_package_data=True,
       description='Feedback via github issues',
       long_description=README,
-      classifiers=['Private :: Do Not Upload'],
+      url='feedback.lostpropertyhq.com',
       author='pete@lostpropertyhq.com',
       author_email='pete@lostpropertyhq.com',
-      url='feedback.lostpropertyhq.com',
       keywords='',
-      package_dir={'': 'feedback'},
-      packages=find_packages('feedback'),
-      include_package_data=True,
+      #package_dir={'': 'feedback'},
       zip_safe=False,
-      )
+      classifiers=['Private :: Do Not Upload'],
+)
